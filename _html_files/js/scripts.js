818a1c3d5104daf86ef4b64d7765dd9c7ff8703f
Kim Bratzel <kim.bratzel@gmail.com>
2014-02-07 10:26:53 +0700

    Added a nice looking cover-flow feature for html output.

--- _html_files/js/scripts.js
+++ _html_files/js/scripts.js
@@ -8,6 +8,19 @@ $(document).ready(function()
         "aaSorting": [[2,'desc']],    //sort by the IMDB rating by default
     });
 
+    $('#coverflow').coverscroll({
+        'titleclass':'coverflow-meta',
+        'selectedclass':'coverflow-selected', 
+        'scrollactive':false,
+        'distribution':1.8,
+        'scalethreshold':1,
+        'step':{ // compressed items on the side are steps
+            'limit': 6, // how many steps should be shown on each side
+            'width': 0, // how wide is the visible section of the step in pixels
+            'scale':false // scale down steps
+        }
+    });
+
 });
 
 
--- tests/unitTests.py
+++ tests/unitTests.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.join(os.path.abspath(pathname), '..', 'src'))
 from main import *
 
 
-class TestSequenceFunctions(unittest.TestCase):
+class UnitTests(unittest.TestCase):
 
     allowedFiletypes = ["tmp","avi","mpg","mpeg","mkv","mp4","divx"]
     matchRegex = "^[^.].+$"
