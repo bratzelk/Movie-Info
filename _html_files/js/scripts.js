e14d2dfa9a1ac45cee429bf1c490c3873cda357d
Kim Bratzel <kim.bratzel@gmail.com>
2014-02-09 11:19:01 +0700

    Made the overflow feature look and behave much nicer. Imported the jquery-ui library which is required by the new coverflow plugin.

--- _html_files/js/scripts.js
+++ _html_files/js/scripts.js
@@ -8,17 +8,34 @@ $(document).ready(function()
         "aaSorting": [[2,'desc']],    //sort by the IMDB rating by default
     });
 
-    $('#coverflow').coverscroll({
-        'titleclass':'coverflow-meta',
-        'selectedclass':'coverflow-selected', 
-        'scrollactive':false,
-        'distribution':1.8,
-        'scalethreshold':1,
-        'step':{ // compressed items on the side are steps
-            'limit': 6, // how many steps should be shown on each side
-            'width': 0, // how wide is the visible section of the step in pixels
-            'scale':false // scale down steps
-        }
+
+    //hide the coverflow by default
+    $("#coverflow").hide();
+    $("#coverflow-meta").hide();
+
+    //show hide the coverflow when they click the link
+    $("#coverflow-toggle").click(function () {
+        $("#coverflow").toggle();
+        $("#coverflow-meta").toggle();
+        //trigger the resize event so it redraws the coverflow
+        $(window).trigger('resize');
+    });
+
+    //Do the actual coverflow
+    $('#coverflow').coverflow({
+        index:          0,
+        density:        1.2,
+        innerOffset:    20,
+        innerScale:     .65,
+        innerAngle:     -55,
+        outerAngle:     -40,
+
+        //add the selected title when they select a new one from the coverflow
+        select:         function(event, cover){
+                            var img = $(cover).children().andSelf().filter('img').last();
+                            $('#coverflow-meta').text(img.data('title') || 'unknown');
+                        }
+    
     });
 
 });
