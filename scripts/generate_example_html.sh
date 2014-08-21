5ffb6dba1481418e96e7bec5f806029353cec7c0
Kim Bratzel <kim.bratzel@gmail.com>
2014-08-21 15:56:17 +1000

    Updated the HTML and the generate script.

--- scripts/generate_example_html.sh
+++ scripts/generate_example_html.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-printf "Generating Example HTML Output... \n\n"
+printf "Running the output generator...\n\n"
 
-python ./src/main.py -d ./tests/testMovieDirectory     -html -limit 500    > example-output.html
 
+printf "Generating Stylesheets... \n"
+./scripts/generate_stylesheets.sh
 printf "..Done. \n\n"
+
+printf "Generating Example HTML Output... \n"
+python ./src/main.py -d ./tests/testMovieDirectory     -html -limit 500    > example-output.html
+printf "..Done. \n\n"
+
 printf "You can now open example-output.html \n\n"
