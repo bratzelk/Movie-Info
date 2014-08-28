723ccf233c637aa62b96074b88614a1771ca6676
Kim Bratzel <kim.bratzel@gmail.com>
2014-08-28 13:08:38 +1000

    Minified CSS files. Moved external CSS to a separate folder so that it is ignored by the GitHub linguist algorithm.

--- movieinfo/scripts/generate_stylesheets.sh
+++ movieinfo/scripts/generate_stylesheets.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-lessc ./_html_files/less/style.less ./_html_files/css/style.css
-lessc ./_html_files/less/table.less ./_html_files/css/table.css
+lessc -x ./_html_files/less/style.less ./_html_files/css/style.css
+lessc -x ./_html_files/less/table.less ./_html_files/css/table.css
